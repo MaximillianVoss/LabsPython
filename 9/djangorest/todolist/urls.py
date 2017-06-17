690978146886233a0d035d5d8c3ef20ff7f6ec79
Александр <fosswolfgang@hotmail.com>
2017-06-17 19:11:50 +0300

    Init

--- 9/djangorest/todolist/urls.py
+++ 9/djangorest/todolist/urls.py
@@ -10,9 +10,9 @@ urlpatterns = {
     url(r'^tags/$', TagCreateView.as_view(), name='tags-list'),
     url(r'^users/$', UserCreateView.as_view(), name="user-list"),
     url(r'^todolists/$', TasklistCreateView.as_view(), name="lists"),
-    url(r'^todolists/(?MyProject<pk>[0-9]+)/$', TasklistDetailsView.as_view(), name="list-detail"),
-    url(r'^todolists/(?MyProject<list_id>[0-9]+)/tasks', TaskCreateView.as_view(), name="list-create"),
-    url(r'^todolists/(?MyProject<list_id>[0-9]+)/tasks/(?MyProject<pk>[0-9]+)', TaskDetailsView.as_view(), name="detail"),
+    url(r'^todolists/(?P<pk>[0-9]+)/$', TasklistDetailsView.as_view(), name="list-detail"),
+    url(r'^todolists/(?P<list_id>[0-9]+)/tasks', TaskCreateView.as_view(), name="tasks"),
+    url(r'^todolists/(?P<list_id>[0-9]+)/tasks/(?P<pk>[0-9]+)', TaskDetailsView.as_view(), name="task-detail"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
